d297d9b614980d4a5eec2106a41b3974810e00cc
Raphaël Denault-Lemaire <raphael.denault-lemaire.1@ulaval.ca>
2024-01-13 09:06:56 -0500

    Simple initial turret operation

--- python/bot.py
+++ python/bot.py
@@ -30,16 +30,26 @@ class Bot:
         # Now crew members at stations should do something!
         operatedTurretStations = [station for station in my_ship.stations.turrets if station.operator is not None]
         for turret_station in operatedTurretStations:
-            possible_actions = [
-                # Charge the turret.
-                TurretChargeAction(turret_station.id),
-                # Aim the turret itself.
-                TurretLookAtAction(turret_station.id, self.get_interception_point(self.get_debris_id(game_message), turret_station, game_message)),
-                # Shoot!
-                TurretShootAction(turret_station.id)
-            ]
-
-            actions.append(random.choice(possible_actions))
+            if turret_station.turretType == "ROCKET":
+                actions.append([
+                    # Charge the turret.
+                    TurretChargeAction(turret_station.id),
+                    # Aim the turret itself.
+                    TurretLookAtAction(turret_station.id,
+                                       self.get_debris_interception_point(self.get_debris_id(game_message),
+                                                                          turret_station, game_message)),
+                    # Shoot!
+                    TurretShootAction(turret_station.id)
+                ])
+            elif turret_station.turretType == "LASER":
+                actions.append([
+                    # Charge the turret.
+                    TurretChargeAction(turret_station.id),
+                    # Aim the turret itself.
+                    TurretLookAtAction(turret_station.id, enemy_ship.position),
+                    # Shoot!
+                    TurretShootAction(turret_station.id)
+                ])
 
         # operatedHelmStation = [station for station in my_ship.stations.helms if station.operator is not None]
         # if operatedHelmStation:
@@ -55,6 +65,7 @@ class Bot:
     def get_debris_id(self, game_message: GameMessage):
         debris = [debris for debris in game_message.debris]
         return random.choice(debris)
+
     def smallestWhichIsntNegativeOrNan(self, numbers):
         smallest = math.inf
         for number in numbers:
@@ -64,7 +75,7 @@ class Bot:
             return 0
         return smallest
 
-    def get_interception_point(self, target: Debris, turret: TurretStation, game_message: GameMessage):
+    def get_debris_interception_point(self, target: Debris, turret: TurretStation, game_message: GameMessage):
         P0 = target.position
         # P0 = Vector(meteor.position.x + meteor.velocity.x, meteor.position.y + meteor.velocity.y)
         V0 = target.velocity
@@ -72,21 +83,21 @@ class Bot:
         P1 = turret.worldPosition
         s1 = game_message.constants.ship.stations.turretInfos[turret.turretType].rocketSpeed
 
-        interception_point = Vector(0, 0)
+        return self.get_interception_point(P0, V0, s0, P1, s1)
 
-        # print(P0, V0, s0, P1, s1)
-        a = (V0.x * V0.x) + (V0.y * V0.y) - (s1 * s1)
-        b = 2 * ((P0.x * V0.x) + (P0.y * V0.y) -
-                 (P1.x * V0.x) - (P1.y * V0.y))
-        c = (P0.x * P0.x) + (P0.y * P0.y) + (P1.x * P1.x) + \
-            (P1.y * P1.y) - (2 * P1.x * P0.x) - (2 * P1.y * P0.y)
+    def get_interception_point(self, p0, v0, s0, p1, s1):
+        a = (v0.x * v0.x) + (v0.y * v0.y) - (s1 * s1)
+        b = 2 * ((p0.x * v0.x) + (p0.y * v0.y) -
+                 (p1.x * v0.x) - (p1.y * v0.y))
+        c = (p0.x * p0.x) + (p0.y * p0.y) + (p1.x * p1.x) + \
+            (p1.y * p1.y) - (2 * p1.x * p0.x) - (2 * p1.y * p0.y)
 
         t1 = (-b + math.sqrt((b * b) - (4 * a * c))) / (2 * a)
         t2 = (-b - math.sqrt((b * b) - (4 * a * c))) / (2 * a)
 
         t = self.smallestWhichIsntNegativeOrNan([t1, t2])
 
-        interception_point = Vector(P0.x + (t * V0.x), P0.y + (t * V0.y))
+        interception_point = Vector(p0.x + (t * v0.x), p0.y + (t * v0.y))
 
         return interception_point
 
